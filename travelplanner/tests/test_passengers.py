f95d822659ebf5ec867fbc84859f2fae45d75604
yue1101 <rmapyli@ucl.ac.uk>
2019-12-27 16:10:31 +0000

    passengers-travis

--- travelplanner/tests/test_passengers.py
+++ travelplanner/tests/test_passengers.py
@@ -7,12 +7,10 @@ def test_walk_time():
     time = person.walk_time()
     assert time == approx(296.4456, abs=1e-4)
 
+
 def test_fail_walk_time():
     with raises(ValueError) as exc_info:
         passenger = Passengers([2, 8], (24, 2), 13)
         if type(passenger.start_point) != tuple:
             raise ValueError('Input(start_point) type is invalid!')
-    assert exc_info.type == ValueError
-
-
-    
+    assert exc_info.type == ValueError  
