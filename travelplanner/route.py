24e584209a977b389122cf37dde4013d5bc78f74
yue1101 <rmapyli@ucl.ac.uk>
2019-12-27 20:31:30 +0000

    route module created

---
+++ travelplanner/route.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+
+class Route:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_routes(self):
+        with open(self.filename) as fn:
+            lines = csv.reader(fn)
+            routes = [(int(line[0]), int(line[1]), line[2])
+                    for line in lines]
+        return routes
+
+    def plot_map(self):
+        route = self.read_routes()
+        max_x = max([n[0] for n in route]) + 5 # adds padding
+        max_y = max([n[1] for n in route]) + 5 
+        grid = np.zeros((max_y, max_x)) 
+        for x, y, stop in route:
+            grid[y, x] = 1
+            if stop:
+                grid[y, x] += 1 
+        _, ax = plt.subplots(1, 1) 
+        ax.pcolor(grid) 
+        ax.invert_yaxis()
+        ax.set_aspect('equal', 'datalim') 
+        plt.show()
+
+    def timetable(self):
+        '''     
+        Generates a timetable for a route as minutes from its first stop. 
+        '''
+        route = self.read_routes()
+        time = 0 
+        stops = {} 
+        for step in route:
+            if step[2]:
+                stops[step[2]] = time
+            time += 10 
+        return stops
+
+    def route_cc(self):
+        '''
+        Converts a set of route into a Freeman chain code
+           3  2  1
+            \ | / 
+        4   - C -   0
+            / | \ 
+           5  6  7 
+        '''
+        route = self.read_routes()
+        start = route[0][:2] 
+        cc = [] 
+        freeman_cc2coord = {0: (1, 0), 
+                            1: (1, -1), 
+                            2: (0, -1), 
+                            3: (-1, -1), 
+                            4: (-1, 0), 
+                            5: (-1, 1), 
+                            6: (0, 1), 
+                            7: (1, 1)} 
+        freeman_coord2cc = {val: key for key,val in freeman_cc2coord.items()} 
+        for b, a in zip(route[1:], route):
+            x_step = b[0] - a[0]
+            y_step = b[1] - a[1]
+            cc.append(str(freeman_coord2cc[(x_step, y_step)])) 
+        return start, ''.join(cc)
+
+        
+            
--- travelplanner/tests/test_passengers.py
+++ travelplanner/tests/test_passengers.py
@@ -2,6 +2,13 @@ from travelplanner.passengers import Passengers
 from pytest import approx, raises
 
 
+def test_constructor():
+    passenger = Passengers((1, 2), (8, 12), 10)
+    assert passenger.start_point == (1, 2)
+    assert passenger.end_point == (8, 12)
+    assert passenger.speed == 10
+
+
 def test_walk_time():
     person = Passengers((2, 8), (24, 2), 13)
     time = person.walk_time()
@@ -10,7 +17,7 @@ def test_walk_time():
 
 def test_fail_walk_time():
     with raises(ValueError) as exc_info:
-        passenger = Passengers([2, 8], (24, 2), 13)
-        if type(passenger.start_point) != tuple:
-            raise ValueError('Input(start_point) type is invalid!')
-    assert exc_info.type == ValueError
+        passenger = Passengers((2, 8), (24, 2), 12)
+        time = passenger.walk_time()
+        if time != approx(296.4456, abs=1e-4):
+            raise ValueError('Wrong inputs of passengers, please check!')
